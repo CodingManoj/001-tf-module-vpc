95d6ddacd481c26ac735d2e8fb33925ab06d1a15
CodingManoj <CodingManoj@gmail.com>
2023-12-17 22:27:31 -0500

    Terraform Auto Push

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,3 +1,4 @@
+# Creating Subnets
 resource "aws_subnet" "main" {
   for_each          = var.subnets
 
@@ -10,5 +11,12 @@ resource "aws_subnet" "main" {
     }
 }
 
-variable "subnets" {}
-variable "vpc_id" {}
+# Creating Route Tables
+resource "aws_route_table" "main" {
+  for_each          = var.subnets
+
+  vpc_id            = var.vpc_id
+    tags = {
+        Name = each.key
+    }
+}
