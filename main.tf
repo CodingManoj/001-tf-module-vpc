a1263a651ae21ad1d5e9747a3147e4e997d3993d
CodingManoj <CodingManoj@gmail.com>
2023-12-17 21:36:26 -0500

    Terraform Auto Push

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "aws_vpc" "main" {
     cidr_block   = var.cidr 
 }
+
+module "subnets" {
+    for_each = var.subnets
+
+    source   = "./subnets"
+    subnets  = each.value
+    vpc_id   = aws_vpc.main.id 
+}
