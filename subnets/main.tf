6341ac1aa497e4000e570d85b0e8e82925921c8d
CodingManoj <CodingManoj@gmail.com>
2023-12-17 22:24:40 -0500

    Terraform Auto Push

--- subnets/main.tf
+++ subnets/main.tf
@@ -5,9 +5,9 @@ resource "aws_subnet" "main" {
   cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
 
-  tags = {
-    Name = each.key
-  }
+    tags = {
+        Name = each.key
+    }
 }
 
 variable "subnets" {}
